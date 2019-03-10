ce69fb75c6096b225f56b8de6c7e762c44c5d06f
unknown <arnkchn@gmail.com>
2019-03-11 00:09:31 +0530

    basic authorisation

--- confusionServer.1/app.js
+++ confusionServer.1/app.js
@@ -4,11 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
 
 var app = express();
+const mongoose = require('mongoose');
 
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -17,35 +25,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-function auth(req, res, next){
-  console.log(req.headers);
-
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error('you are not authorised');
-    res.setHeader('www-Authenticate', "Basic");
-    err.status = 401;
-    return next(err);
-  }
-
-  var auth = new Buffer(authHeader.split('')[1], "base64").toString.split(':');
-  var username  = auth[0];
-  var password = auth[0];
-  if(username === 'admin' && password === 'password'){
-    next();
-  }else{
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Athenticate', "Basic");
-    err.status = 401
-    return next(err);
-  }
-}
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/dishes', dishRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- confusionServer.1/models/dishes.js
+++ confusionServer.1/models/dishes.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
 
 var commentSchema = new Schema({
     rating:  {
@@ -19,8 +21,7 @@ var commentSchema = new Schema({
 }, {
     timestamps: true
 });
-
-var dishSchema = new Schema({
+const dishSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -30,11 +31,31 @@ var dishSchema = new Schema({
         type: String,
         required: true
     },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
     comments:[commentSchema]
 }, {
     timestamps: true
 });
-
 var Dishes = mongoose.model('Dish', dishSchema);
 
 module.exports = Dishes;
