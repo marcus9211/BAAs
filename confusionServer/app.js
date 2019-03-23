8e0e620d41f184b4e9304f795c299a60f0100322
unknown <arnkchn@gmail.com>
2019-03-23 19:00:05 +0530

    use of session

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const createError = require('http-errors'),
+  express = require('express'),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  logger = require('morgan'),
+  session = require("express-session"),
+  FileStore = require("session-file-store")(session),
+  indexRouter = require('./routes/index'),
+  dishRouter = require("./routes/dishRouter"),
+  usersRouter = require('./routes/users'),
+  app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,44 +17,62 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-function auth(req, res, next){
-  console.log(req.headers);
+// app.use(cookieParser("12345-67890-09876-54321"));
+app.use(session({
+  name: "session-id",
+  secret: "12345-67890-09876-54321",
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}))
+function auth(req, res, next) {
+  console.log(req.session);
+  if (!req.session.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error('you are not authorised');
+      res.setHeader('www-Authenticate', "Basic");
+      err.status = 401;
+      return next(err);
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], "base64").toString().split(':');
 
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error('you are not authorised');
-    res.setHeader('www-Authenticate', "Basic");
-    err.status = 401;
-    return next(err);
-  }
-
-  var auth = new Buffer(authHeader.split('')[1], "base64").toString.split(':');
-  var username  = auth[0];
-  var password = auth[0];
-  if(username === 'admin' && password === 'password'){
-    next();
-  }else{
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Athenticate', "Basic");
-    err.status = 401
-    return next(err);
+    var username = auth[0];
+    var password = auth[1];
+    if (username === 'admin' && password === 'password') {
+      req.session.user = "admin";
+      next();//authorized
+    } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Athenticate', "Basic");
+      err.status = 401
+      return next(err);
+    }
+  } else {
+    if (req.session.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   }
 }
-
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/dishes", dishRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
